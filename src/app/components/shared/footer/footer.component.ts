9148a4bffe2dc088dcea302f00b22991699f062f
Clarhence Oliveros <134087492+Ashrennn@users.noreply.github.com>
2025-04-25 12:02:33 +0800

    Shared Footer Added

---
+++ src/app/components/shared/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive]
+})
+export class FooterComponent {
+  constructor() { }
+}
