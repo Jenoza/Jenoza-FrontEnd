037340ba42bd66f5e3b9b798a08ac7ca0f745296
Clarhence Oliveros <134087492+Ashrennn@users.noreply.github.com>
2025-04-25 11:59:49 +0800

    Added Footer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/shared/navigation/navigation.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavigationComponent,
+    FooterComponent
+  ],
   template: `
     <div class="app-container">
       <jnz-navigation></jnz-navigation>
       <main>
         <router-outlet></router-outlet>
       </main>
+      <app-footer></app-footer>
     </div>
   `,
   styles: [`
